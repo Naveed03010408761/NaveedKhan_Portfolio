bc016f876abab9d97e1d8fd550d9e3813760f9a3
Naveed03010408761 <akmal030104087@gmail.com>
2025-04-24 09:10:27 +0500

    Update something in the project

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -8,9 +8,10 @@ export const Home = () => {
     >
       <RevealOnScroll>
       <div className="text-center z-10 px-4">
-        <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 t0-purple-600 bg-clip-text text-transparent leading-right">
-          Hi, I'm Naveed Khan
-        </h1>
+      <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-blue-600 bg-clip-text text-transparent leading-tight">
+  Hi, I'm Naveed Khan
+</h1>
+
 
         <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
           As a Frontend Developer with hands-on experience in HTML, CSS,
@@ -20,20 +21,19 @@ export const Home = () => {
         </p>
 
         <div className="flex justify-center space-x-4 ">
-          <a
-            href="#projects"
-            className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,24,0.4)"
-          >
-            {" "}
-            View Projects
-          </a>
-          <a
-            href="#contact"
-            className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,24,0.4)"
-          >
-            {" "}
-            Contact Me
-          </a>
+        <a
+  href="#projects"
+  className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
+>
+  View Projects
+</a>
+<a
+  href="#contact"
+  className="bg-teal-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(48,211,176,0.4)]"
+>
+  Contact Me
+</a>
+
         </div>
       </div>
       </RevealOnScroll>
